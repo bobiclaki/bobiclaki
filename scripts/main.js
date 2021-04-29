9230a63801723036e6b7051572b7b24cdbe24278
bo-bic-laki <nikita134222@gmail.com>
2021-04-29 17:07:55 +0300

    Load_1
    1

---
+++ scripts/main.js
@@ -0,0 +1,178 @@
+class TextScramble {
+    constructor(el) {
+        this.el = el
+        this.chars = '!<>-_\\/[]{}—=+*^?#________'
+        this.update = this.update.bind(this)
+    }
+    setText(newText) {
+        const oldText = this.el.innerText
+        const length = Math.max(oldText.length, newText.length)
+        const promise = new Promise((resolve) => this.resolve = resolve)
+        this.queue = []
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || ''
+            const to = newText[i] || ''
+            const start = Math.floor(Math.random() * 40)
+            const end = start + Math.floor(Math.random() * 40)
+            this.queue.push({ from, to, start, end })
+        }
+        cancelAnimationFrame(this.frameRequest)
+        this.frame = 0
+        this.update()
+        return promise
+    }
+    update() {
+        let output = ''
+        let complete = 0
+        for (let i = 0, n = this.queue.length; i < n; i++) {
+            let { from, to, start, end, char } = this.queue[i]
+            if (this.frame >= end) {
+                complete++
+                output += to
+            } else if (this.frame >= start) {
+                if (!char || Math.random() < 0.28) {
+                    char = this.randomChar()
+                    this.queue[i].char = char
+                }
+                output += `<span class="dud">${char}</span>`
+            } else {
+                output += from
+            }
+        }
+        this.el.innerHTML = output
+        if (complete === this.queue.length) {
+            this.resolve()
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update)
+            this.frame++
+        }
+    }
+    randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)]
+    }
+}
+
+const phrases = [
+    'bobiclaki.xyz',
+    '#helloworld',
+    'SOON',
+    'Bye Bye'
+]
+  
+const el = document.querySelector('.text')
+const fx = new TextScramble(el)
+  
+let counter = 0
+const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 800)
+    })
+    counter = (counter + 1) % phrases.length
+}
+next()
+
+particlesJS("logo", {
+    "particles": {
+        "number": {
+        "value": 120,
+        "density": {
+            "enable": true,
+            "value_area": 700
+        }
+        },
+        "color": {
+        "value": `#ffffff`
+        },
+        "shape": {
+        "type": "circle",
+        "stroke": {
+            "width": 0,
+            "color": `#ffffff`
+        },
+        "polygon": {
+            "nb_sides": 15
+        }
+        },
+        "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+            "enable": false,
+            "speed": 1.5,
+            "opacity_min": 0.15,
+            "sync": false
+        }
+        },
+        "size": {
+        "value": 2.5,
+        "random": false,
+        "anim": {
+            "enable": true,
+            "speed": 2,
+            "size_min": 0.15,
+            "sync": false
+        }
+        },
+        "line_linked": {
+        "enable": true,
+        "distance": 120,
+        "color": `#ffffff`,
+        "opacity": 0.3,
+        "width": 1
+        },
+        "move": {
+        "enable": true,
+        "speed": 2.5,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+        }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+        "onhover": {
+            "enable": false,
+            "mode": "repulse"
+        },
+        "onclick": {
+            "enable": false,
+            "mode": "push"
+        },
+        "resize": true
+        },
+        "modes": {
+        "grab": {
+            "distance": 400,
+            "line_linked": {
+            "opacity": 1
+            }
+        },
+        "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+        },
+        "repulse": {
+            "distance": 200,
+            "duration": 0.4
+        },
+        "push": {
+            "particles_nb": 4
+        },
+        "remove": {
+            "particles_nb": 2
+        }
+        }
+    },
+    "retina_detect": true
+});
